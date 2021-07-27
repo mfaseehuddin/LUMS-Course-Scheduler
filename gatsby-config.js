42767f9b76ab4f615cfd5c301b6670a6d54e2e27
Muhammad Faseeh Ud Din <mfaseehuddin2001@gmail.com>
2021-07-27 07:19:11 +0500

    Commit 2 -- Completed The Search, Select and, Remove Feature.
    Next: Will Add Day and Time Parsing Systems so that courses can be placed on their proper locations.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,16 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "LUMS Course Scheduler",
+    title: "LUMS Course Scheduler | Fall 2021",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: ["gatsby-plugin-sass",
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name:  `src`,
+      path: `${__dirname}/src/data`,
+    },
+  },
+  `gatsby-transformer-csv`,
+  ],
 };
---
+++ src/hooks/useCourseQuery.js
@@ -0,0 +1,21 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+export const useCourseQuery = () => {
+    const {allF2021Csv} = useStaticQuery(
+        graphql`
+            query allCourses {
+                allF2021Csv {
+                    nodes {
+                        id
+                        Course_Code
+                        Course_Title
+                        Instructor_s_
+                        Cr_Hrs
+                        Day_and_Time
+                    }
+                }
+            }
+        `
+    )
+    return allF2021Csv
+}
